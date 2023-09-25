6ec165ad33742dd686cfe9176636ef77cdf2539a
Nuestro Nombre <nuestromail@correo.com>
2023-09-24 23:19:19 -0500

    migrts1

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 const express = require('express')
-const cors_ = require('cors')
-const bodyParser = require('body-parser')
-const RouterUsers = require('./src/Routes/Users')
-const RouterApp = require('./src/Routes/App')
-
-//require('./src/DBase/Firebase/ConexionFirebase')
-
 const App = express()
 
-// middlewares
-App.use(
-  express.json({
-    limit: '35mb'
-  })
-)
-App.use(
-  cors_({
-    origin: 'https://arcontroller.vercel.app/' || 'http://localhost:3000'
-  })
-)
-
-App.use('/home', (req, res) => {
-  res.send('Cpanel aqui')
+App.listen(3010, () => {
+  console.log('====================================')
+  console.log('server listen')
+  console.log('====================================')
 })
 
-App.use('/api/arcontroller/', RouterUsers)
-App.use('/api/arcontroller/', RouterApp)
+// const express = require('express')
+// const cors_ = require('cors')
+// const bodyParser = require('body-parser')
+// const RouterUsers = require('./src/Routes/Users')
+// const RouterApp = require('./src/Routes/App')
 
-App.set('port', process.env.PORT || 2023)
+// //require('./src/DBase/Firebase/ConexionFirebase')
 
-App.listen(App.get('port'), () => {
-  console.log(`servidor levantado en puerto ${App.get('port')}`)
-})
+// const App = express()
+
+// // middlewares
+// App.use(
+//   express.json({
+//     limit: '35mb'
+//   })
+// )
+// App.use(
+//   cors_({
+//     origin: 'https://arcontroller.vercel.app/' || 'http://localhost:3000'
+//   })
+// )
+
+// App.use('/home', (req, res) => {
+//   res.send('Cpanel aqui')
+// })
+
+// App.use('/api/arcontroller/', RouterUsers)
+// App.use('/api/arcontroller/', RouterApp)
+
+// App.set('port', process.env.PORT || 2023)
+
+// App.listen(App.get('port'), () => {
+//   console.log(`servidor levantado en puerto ${App.get('port')}`)
+// })

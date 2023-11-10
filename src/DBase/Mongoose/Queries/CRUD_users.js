1ca89a6b2134c15bac2fa9ac0b8529d8a0ce2756
Eric Martinez Acelas <emartinezacelas@gmail.com>
2023-11-09 20:47:44 -0500

    ming

--- src/DBase/Mongoose/Queries/CRUD_users.js
+++ src/DBase/Mongoose/Queries/CRUD_users.js
@@ -1,27 +1,22 @@
-const users_schema = require('../Models/users_schema')
-const prodct_schema = require('../Models/prodct_schema')
+const users_schema = require("../Models/users_schema");
+const prodct_schema = require("../Models/prodct_schema");
 
-const FindByUSUandPsw = async (user, pswLogin, clav_prodct) => {
-  if (clav_prodct === 'PM') {
-    console.log('buscar como PM')
-    return await users_schema.findOne({ user, pswLogin }).exec()
+const FindByUSUandPsw = async (user, pswLogin, clav_prodct, rol) => {
+  let findProdct = await prodct_schema.findOne({ clav_prodct }).exec();
+  if (findProdct !== null) {
+    console.log(`Buscando...`);
+    return await users_schema.findOne({ user, pswLogin, rol }).exec();
   } else {
-    console.log('buscar como PO')
-    let findProdct = await prodct_schema.findOne({ clav_prodct }).exec()
-    if (findProdct !== null) {
-      return await users_schema.findOne({ user, pswLogin, rol: 'PO' }).exec()
-    } else {
-      console.log('clave erronea')
-      return await 'bad'
-    }
+    console.log("clave erronea");
+    return await "bad";
   }
-}
+};
 const RegtrByUSUandPsw = async (user, pswLogin, rol) => {
-  return await new users_schema({ user, pswLogin, rol }).save()
-}
+  return await new users_schema({ user, pswLogin, rol }).save();
+};
 
 const FindAndUpdateToken = (id, _token) => {
-  return users_schema.findByIdAndUpdate({ _id: id }, { token: _token })
-}
+  return users_schema.findByIdAndUpdate({ _id: id }, { token: _token });
+};
 
-module.exports = { FindByUSUandPsw, RegtrByUSUandPsw, FindAndUpdateToken }
+module.exports = { FindByUSUandPsw, RegtrByUSUandPsw, FindAndUpdateToken };

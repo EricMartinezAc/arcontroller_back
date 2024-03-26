fdf822d44799955e240572f224f4594bb6d161d3
Eric Martinez Acelas <emartinezacelas@gmail.com>
2024-03-25 19:51:46 -0500

    jshdyok

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const express = require('express')
-const cors_ = require('cors')
-const bodyParser = require('body-parser')
-const RouterUsers = require('./src/Routes/Users')
-const RouterApp = require('./src/Routes/App')
+const express = require("express");
+const cors_ = require("cors");
+const RouterAuthReg = require("./src/Routes/AuthReg.js");
+const RouterApp = require("./src/Routes/App");
 
 //require('./src/DBase/Firebase/ConexionFirebase')
 
-const App = express()
+const App = express();
 
 // middlewares
 App.use(
   express.json({
-    limit: '35mb'
+    limit: "35mb",
   })
-)
+);
 App.use(
   cors_({
-    origin: '*'
+    origin: "*",
   })
-)
+);
 
-App.use('/home', (req, res) => {
-  console.log('====================================')
-  console.log('Cpanel')
-  console.log('====================================')
-  res.send('Cpanel aqui')
-})
+//Route 404
+App.use("*", (req, res) => {
+  console.error(`404 from ${req.url} by ${req.ip}`);
+  res.status(404);
+});
 
-App.use('/api/arcontroller/', RouterUsers)
-App.use('/api/arcontroller/', RouterApp)
+//Routes
+App.use("/api/arcontroller/", RouterAuthReg);
+App.use("/api/arcontroller/", RouterApp);
 
-App.set('port', process.env.PORT || 2023)
+App.set("port", process.env.PORT || 2023);
 
-App.listen(App.get('port'), () => {
-  console.log(`servidor levantado en puerto ${App.get('port')}`)
-})
+App.listen(App.get("port"), () => {
+  console.log(`servidor levantado en puerto ${App.get("port")}`);
+});
--- src/DBase/Mongoose/Models/Localidades_schema.js
+++ src/DBase/Mongoose/Models/Localidades_schema.js
@@ -1,3 +1,48 @@
 const mongoose = require("mongoose");
 
-const Localidades_schema = mongoose.Schema({});
+const Localidades_schema = mongoose.Schema({
+  id_localidades: {
+    type: String,
+    required: false,
+  },
+  nombre_localidades: {
+    type: String,
+    required: false,
+  },
+  pais_localidades: {
+    type: String,
+    required: false,
+  },
+  ciudad_localidades: {
+    type: String,
+    required: false,
+  },
+  dpto_localidades: {
+    type: String,
+    required: false,
+  },
+  direccion_localidades: {
+    type: String,
+    required: false,
+  },
+  contact_localidades: {
+    type: String,
+    required: false,
+  },
+  email_localidades: {
+    type: String,
+    required: false,
+  },
+  fileInput_proveedores: {
+    type: String,
+    required: false,
+  },
+  fileInput_zonas: {
+    type: String,
+    required: false,
+  },
+  fileImgLocalidades: {
+    type: String,
+    required: false,
+  },
+});
